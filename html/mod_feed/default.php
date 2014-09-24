3601841e7a7fc7dd176f4e682282837ddb9cd3ba
voj <jeturian@gmail.com>
2014-09-24 13:52:02 +0800

    Fix feed error

--- html/mod_feed/default.php
+++ html/mod_feed/default.php
@@ -6,112 +6,123 @@
  * @copyright    Copyright (C) 2009 - 2012 Matt Thomas. All rights reserved.
  * @license        GNU/GPL v2 or later http://www.gnu.org/licenses/gpl-2.0.html
  */
+?>
 
-if ($feed != false) {
+<?php
+//echo '<pre>$feed = '.print_r($feed, 1).'</pre>';
+?>
+
+<?php
+if (!empty($feed) && is_string($feed))
+{
+  echo $feed;
+} // !empty($feed) && is_string($feed))
+else
+{
+	if ($feed != false){
     //image handling
-    $iUrl = isset($feed->image->url) ? $feed->image->url : null;
-    $iTitle = isset($feed->image->title) ? $feed->image->title : null;
+      $iUrl	= isset($feed->image)	? $feed->image	: null;
+      $iTitle = isset($feed->imagetitle) ? $feed->imagetitle : null;
     ?>
-<div style="direction: <?php echo $rssrtl ? 'rtl' : 'ltr'; ?>; text-align: <?php echo $rssrtl ? 'right' : 'left'; ?> ! important" class="feed<?php echo $moduleclass_sfx; ?>">
-    <?php
-    // feed title
-    if (!is_null($feed->title) && $params->get('rsstitle', 1)) {
+    <div
+      style="direction: <?php echo $rssrtl ? 'rtl' : 'ltr'; ?>; text-align: <?php echo $rssrtl ? 'right' : 'left'; ?> ! important"
+      class="feed<?php echo $moduleclass_sfx; ?>">
+      <?php
+      // feed title
+      if (!is_null($feed->title) && $params->get('rsstitle', 1)){
         ?>
 
         <h4>
-            <a href="<?php echo str_replace('&', '&amp', $feed->link); ?>" target="_blank">
-                <?php echo $feed->title; ?></a>
+          <a href="<?php echo str_replace('&', '&amp', $rssurl); ?>"
+             target="_blank">
+            <?php echo $feed->title; ?></a>
         </h4>
 
-        <?php
-    }
-
-    // feed description
-    if ($params->get('rssdesc', 1)) {
+      <?php
+      }
+      // Feed description
+      if ($params->get('rssdesc', 1)){
         ?>
         <?php echo $feed->description; ?>
 
-        <?php
-    }
+      <?php
+      }
 
-    // feed image
-    if ($params->get('rssimage', 1) && $iUrl) {
+      // feed image
+      if ($params->get('rssimage', 1) && $iUrl){
         ?>
-        <img src="<?php echo $iUrl; ?>" alt="<?php echo @$iTitle; ?>" />
+        <img src="<?php echo $iUrl; ?>" alt="<?php echo @$iTitle; ?>"/>
 
-        <?php
-    }
+      <?php
+      }
+      ?>
 
-    $actualItems = count($feed->items);
-    $setItems = $params->get('rssitems', 5);
-
-    if ($setItems > $actualItems) {
-        $totalItems = $actualItems;
-    } else {
-        $totalItems = $setItems;
-    }
-    ?>
-
-    <ol class="newsfeed<?php echo $params->get('moduleclass_sfx'); ?>">
+      <ol class="newsfeed<?php echo $params->get('moduleclass_sfx'); ?>">
         <?php
         $words = $params->def('word_count', 0);
-        for ($j = 0; $j < $totalItems; $j++) {
-            $currItem = & $feed->items[$j];
-            // item title
-            ?>
-            <li class="newsfeed-item">
-                <?php    if (!is_null($currItem->get_link())) {
-                ?>
-                <?php if (!is_null($feed->title) && $params->get('rsstitle', 1)) {
-                    echo '<h5 class="feed-link">';
-                }
-                else {
-                    echo '<h4 class="feed-link">';
-                }
-                ?>
+        for ($j = 0; $j < $params->get('rssitems', 5); $j++){
+					if (!$feed->offsetExists($j))
+					{
+						break;
+					}
+          $currItem = $feed[$j];
+          // item title
+          ?>
+          <li class="newsfeed-item">
+            <?php    if (!is_null($currItem->uri)){
+              ?>
+              <?php if (!is_null($feed->title) && $params->get('rsstitle', 1)){
+                echo '<h5 class="feed-link">';
+              }
+              else{
+                echo '<h4 class="feed-link">';
+              }
+              ?>
 
-                <a href="<?php echo $currItem->get_link(); ?>" target="_blank">
-                    <?php echo $currItem->get_title(); ?></a>
-                <?php if (!is_null($feed->title) && $params->get('rsstitle', 1)) {
-                    echo '</h5>';
-                }
-                else {
-                    echo '</h4>';
-                }
-                ?>
-                <?php
+              <a href="<?php echo $currItem->uri; ?>" target="_blank">
+                <?php echo $currItem->title; ?></a>
+              <?php if (!is_null($feed->title) && $params->get('rsstitle', 1)){
+                echo '</h5>';
+              }
+              else{
+                echo '</h4>';
+              }
+              ?>
+            <?php
             }
 
-                // item description
-                if ($params->get('rssitemdesc', 1)) {
-                    // item description
-                    $text = $currItem->get_description();
-                    $text = str_replace('&apos;', "'", $text);
-                    $text = strip_tags($text);
-                    // word limit check
-                    if ($words) {
-                        $texts = explode(' ', $text);
-                        $count = count($texts);
-                        if ($count > $words) {
-                            $text = '';
-                            for ($i = 0; $i < $words; $i++) {
-                                $text .= ' ' . $texts[$i];
-                            }
-                            $text .= '...';
-                        }
-                    }
-                    ?>
-
-                    <p><?php echo $text; ?></p>
-
-                    <?php
+            // item description
+            if ($params->get('rssitemdesc', 1)){
+              // item description
+              $text = $currItem->description;
+              $text = str_replace('&apos;', "'", $text);
+              $text = strip_tags($text);
+              // word limit check
+              if ($words){
+                $texts = explode(' ', $text);
+                $count = count($texts);
+                if ($count > $words){
+                  $text = '';
+                  for ($i = 0; $i < $words; $i++){
+                    $text .= ' ' . $texts[$i];
+                  }
+                  $text .= '...';
                 }
-                ?>
-            </li>
+              }
+              ?>
+
+              <p><?php echo $text; ?></p>
+
             <?php
+            }
+            ?>
+          </li>
+        <?php
         }
         ?>
-    </ol>
-</div>
-<?php }
-
+      </ol>
+    </div>
+  <?php
+  }
+} // !empty($feed) && is_string($feed))
+?>
